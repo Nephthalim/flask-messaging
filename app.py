8377b2c586e5a9b8c55ef86f86f7047c11ae03af
Nephthalim Abebe <nephthalimabebe@Nephthalims-MacBook-Pro.local>
2021-08-12 14:55:41 -0400

    Added worker

--- app.py
+++ app.py
@@ -80,8 +80,4 @@ worker = Worker
 
 
 if __name__ == "__main__":
-    from gevent import pywsgi
-    from geventwebsocket.handler import WebSocketHandler
-    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
-    server.serve_forever()
-    # socketio.run(app)
+    socketio.run(app)
